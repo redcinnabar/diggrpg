34090be214405aff4a66786ed4b1be1172aa2bee
Dmitriy Gonzha <dima0012@inbox.ru>
2018-12-02 21:17:16 +0300

    Added -w for windowed mode

--- main.c
+++ main.c
@@ -49,6 +49,11 @@ BITMAP *load_tile_bitmap(tile_t *t, unsigned char fg, unsigned char bg)
 int main(int argc, char *argv[])
 {
 	BITMAP *image;
+	int gfx_mode;
+	if (argc == 2 && strcmp(argv[1], "-w") == 0)
+		gfx_mode = GFX_AUTODETECT_WINDOWED;
+	else
+		gfx_mode = GFX_AUTODETECT;
 
 	/* initialise everything */
 	if (allegro_init() != 0) {
@@ -60,7 +65,7 @@ int main(int argc, char *argv[])
 	install_timer();
 
 	set_color_depth(8);
-	if (set_gfx_mode(GFX_AUTODETECT, 800, 600, 0, 0) != 0) {
+	if (set_gfx_mode(gfx_mode, 800, 600, 0, 0) != 0) {
 		if (set_gfx_mode(GFX_SAFE, 640, 480, 0, 0) != 0) {
 			set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
 			allegro_message("Unable to set any graphic mode\n%s\n", allegro_error);
